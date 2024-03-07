c2294dbd5946d94446b2819379573de379f25ff7
Chicken Head <Chicken Head@DESKTOP-6RVSQM3.mshome.net>
2024-03-07 16:24:14 +0900

    first commit

---
+++ jj/src/main/java/com/tjoeun/jj/JjApplication.java
@@ -0,0 +1,13 @@
+package com.tjoeun.jj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjApplication.class, args);
+	}
+
+}
