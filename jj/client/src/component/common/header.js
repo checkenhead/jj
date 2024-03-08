d9dff474478a2908731b3bfd04655a03dbc69993
Chicken Head <Chicken Head@DESKTOP-6RVSQM3>
2024-03-08 18:58:15 +0900

    header

--- jj/client/src/component/common/header.js
+++ jj/client/src/component/common/header.js
@@ -1,24 +1,53 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import ImgLogo from '../../images/logo.png';
 import ImgHome from '../../images/home.png';
+import ImgUser from '../../images/user.png';
+import ImgSearch from '../../images/search.png';
+import ImgMessage from '../../images/message.png';
+import ImgPost from '../../images/post.png';
+import ImgLogout from '../../images/logout.png';
 
 function Header() {
   return (
     <div className="wrap_header">
       <div className="logo">
-        <img src={ImgLogo}/>
+        <img src={ImgLogo} />
       </div>
       <nav>
-        <Link to="">Home</Link>
-        <Link to="">My page</Link>
-        <Link to="">Search</Link>
-        <Link to="">Message</Link>
-        <button><img src=""/>Post</button>
-        <button>Logout</button>
+        <div className="row">
+          <Link to="">
+            <img src={ImgHome} className="icon" /><span className="name">Home</span>
+          </Link>
+        </div>
+        <div className="row">
+          <Link to="">
+            <img src={ImgUser} className="icon" /><span className="name">My page</span>
+          </Link>
+        </div>
+        <div className="row">
+          <Link to="">
+            <img src={ImgSearch} className="icon" /><span className="name">Search</span>
+          </Link>
+        </div>
+        <div className="row">
+          <Link to="">
+            <img src={ImgMessage} className="icon" /><span className="name">Message</span>
+          </Link>
+        </div>
+        <div className="row">
+          <button>
+            <img src={ImgPost} className="icon" /><span className="name">Post</span>
+          </button>
+        </div>
+        <div className="row btn_logout">
+          <button>
+            <img src={ImgLogout} className="icon" /><span className="name">Logout</span>
+          </button>
+        </div>
       </nav>
-    </div>
+    </div >
   )
 }
 
