fe282a31d9d3f2ba385ba2f05dc02f7696c74571
Chicken Head <Chicken Head@DESKTOP-6RVSQM3>
2024-03-12 09:48:20 +0900

    feed post

--- jj/client/src/component/feed/feeds.js
+++ jj/client/src/component/feed/feeds.js
@@ -1,102 +1,106 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import Post from './post';
+
 import Img from '../../images/Koala.jpg';
 
+
 function Feeds() {
-  return (
-    <>
-    <div className="instant_post">
-      <div className="content">
-        <textarea></textarea>
-      </div>
-      <div className="btn">버튼들</div>
-    </div>
-    <div className="wrap_feeds">
-      <div className="feed">
-        <div className="feed_head">
-          <div>profileimg</div>
-          <div>nickname</div>
-          <div>timestamp</div>
-        </div>
-        <div className="feed_img"><img src={Img}/></div>
-        <div className="feed_content">ContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContent</div>
-        <div className="feed_icon">123</div>
-        <div className="feed_reply">456</div>
-      </div>
-      <div className="feed">
-        <div className="feed_head">
-          테스트 피드 #2
-        </div>
-        <div className="feed_img"><img src={Img}/></div>
-        <div className="feed_icon"></div>
-        <div className="feed_reply"></div>
-      </div>
-      <div className="feed">
-        <div className="feed_head">
-          테스트 피드 #3
-        </div>
-        <div className="feed_img"><img src={Img}/></div>
-        <div className="feed_icon"></div>
-        <div className="feed_reply"></div>
-      </div>
-      <div className="feed">
-        <div className="feed_head">
-          테스트 피드 #4
-        </div>
-        <div className="feed_img"><img src={Img}/></div>
-        <div className="feed_icon"></div>
-        <div className="feed_reply"></div>
-      </div>
-      <div className="feed">
-        <div className="feed_head">
-          테스트 피드 #5
-        </div>
-        <div className="feed_img"><img src={Img}/></div>
-        <div className="feed_icon"></div>
-        <div className="feed_reply"></div>
-      </div>
-      <div className="feed">
-        <div className="feed_head">
-          테스트 피드 #6
-        </div>
-        <div className="feed_img"><img src={Img}/></div>
-        <div className="feed_icon"></div>
-        <div className="feed_reply"></div>
-      </div>
-      <div className="feed">
-        <div className="feed_head">
-          테스트 피드 #7
-        </div>
-        <div className="feed_img"><img src={Img}/></div>
-        <div className="feed_icon"></div>
-        <div className="feed_reply"></div>
-      </div>
-      <div className="feed">
-        <div className="feed_head">
-          테스트 피드 #8
-        </div>
-        <div className="feed_img"><img src={Img}/></div>
-        <div className="feed_icon"></div>
-        <div className="feed_reply"></div>
-      </div>
-      <div className="feed">
-        <div className="feed_head">
-          테스트 피드 #9
-        </div>
-        <div className="feed_img"><img src={Img}/></div>
-        <div className="feed_icon"></div>
-        <div className="feed_reply"></div>
-      </div>
-      <div className="feed">
-        <div className="feed_head">
-          테스트 피드 #10
-        </div>
-        <div className="feed_img"><img src={Img}/></div>
-        <div className="feed_icon"></div>
-        <div className="feed_reply"></div>
-      </div>
-    </div>
-    </>
-  )
+
+    return (
+        <>
+            <Post />
+            <div id="main_tab">
+                <button className="link">For you</button>
+                <button className="link">Following</button>
+            </div>
+            <div className="wrap_feeds">
+                <div className="feed">
+                    <div className="feed_head">
+                        <div>profileimg</div>
+                        <div>nickname</div>
+                        <div>timestamp</div>
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_content">ContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContent</div>
+                    <div className="feed_icon">123</div>
+                    <div className="feed_reply">456</div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #2
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #3
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #4
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #5
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #6
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #7
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #8
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #9
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #10
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Feeds
