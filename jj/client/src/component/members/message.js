8be2f7df3a1373ca8def16b70ab5c255602e8c5b
JAVA <JAVA@DESKTOP-AG705RJ>
2024-03-12 15:18:06 +0900

    message

--- jj/client/src/component/members/message.js
+++ jj/client/src/component/members/message.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from '../common/header';
 import Sub from '../common/sub';
 import '../../style/members/message.css';
+import { Link, useNavigate } from 'react-router-dom'
+import ImgFriend from '../../images/crayon.jpg';
 
 function message() {
   return (
@@ -9,35 +11,68 @@ function message() {
     <header><Header/></header>
     <main>
 
-        <div className='messagebox'>
-        
-          <div className='friend'>
-              
-          </div>
-          <hr />
-          <div className='message'>
-              <div className='row received'>
-                  
-              </div>
-              <div className='row sent'>
-                  
-              </div>
-              <div className='row sent'>
-                  
-              </div>
-              <div className='row received'>
-                  
-              </div>
-              <div className='inputmessage'>
-              <input type='text' />
-              <button id="sendbtn">SEND</button>
-              </div>
-              
+      <div className='wrap_message'>
 
+        <div className='allmsg'>
+          <div className='msg'>
+            <div className='msg received'>ABCDEABC</div>
           </div>
+          <div className='msg'>
+            <div className='msg sent'>ABCDEABCDEABCDEABCDEABCDEABCDE</div>
           </div>
+          <div className='msg'>
+            <div className='msg sent'>ABCDEABCDEABCDEABCDEABCDEABCDE</div>
+          </div>
+
+          {/* <div className='msg'>
+            <div className='msg received'>ABCDEABC</div>
+          </div>
+          <div className='msg'>
+            <div className='msg sent'>ABCDEABCDEABCDEABCDEABCDEABCDE</div>
+          </div>
+          <div className='msg'>
+            <div className='msg sent'>ABCDEABCDEABCDEABCDEABCDEABCDE</div>
+          </div><div className='msg'>
+            <div className='msg received'>ABCDEABC</div>
+          </div>
+          <div className='msg'>
+            <div className='msg sent'>ABCDEABCDEABCDEABCDEABCDEABCDE</div>
+          </div>
+          <div className='msg'>
+            <div className='msg sent'>ABCDEABCDEABCDEABCDEABCDEABCDE</div>
+          </div><div className='msg'>
+            <div className='msg received'>ABCDEABC</div>
+          </div>
+          <div className='msg'>
+            <div className='msg sent'>ABCDEABCDEABCDEABCDEABCDEABCDE</div>
+          </div>
+          <div className='msg'>
+            <div className='msg sent'>ABCDEABCDEABCDEABCDEABCDEABCDE</div>
+          </div><div className='msg'>
+            <div className='msg received'>ABCDEABC</div>
+          </div>
+          <div className='msg'>
+            <div className='msg sent'>ABCDEABCDEABCDEABCDEABCDEABCDE</div>
+          </div>
+          <div className='msg'>
+            <div className='msg sent'>ABCDEABCDEABCDEABCDEABCDEABCDE</div>
+          </div> */}
+
+          <div className='msgbtn'>
+          <input type='text' className='inputmsg' />
+          <button>SEND</button>
+        </div>
+          </div>
+
+
+
+
+       
+
+      </div>
 
     </main>
+    
     <aside id="aside"><Sub/></aside>
 </div>
   )
