9f2df8ca1ff6400f419ac01bd1fabcd9d76bc00d
connie0714 <ok45909@gmail.com>
2024-03-13 20:35:39 +0900

    feed emoji 수정

--- jj/client/src/component/feed/post.js
+++ jj/client/src/component/feed/post.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import Modal from "react-modal";
 import { useSelector } from 'react-redux';
@@ -80,6 +80,8 @@ function Post() {
             setEmojiStyle({ display: 'block' });
         }
     }
+
+    useEffect(() => {}, [content]);
 
     return (
         <div className="post">
