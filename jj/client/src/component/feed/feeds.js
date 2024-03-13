d3410c85db3dc2c20a08c969fe781ad259cbd3fa
connie <ok45909@gmail.com>
2024-03-13 09:10:05 +0900

    Revert "Merge branch 'main' of https://github.com/checkenhead/jj.git into main"
    This reverts commit 052fcc733a1ca52fd31d6a041bf43e0301a6a93d, reversing
    changes made to 8be2f7df3a1373ca8def16b70ab5c255602e8c5b.

--- jj/client/src/component/feed/feeds.js
+++ jj/client/src/component/feed/feeds.js
@@ -1,27 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 
 import Post from './post';
-import Feed from './feed';
+
+import Img from '../../images/Koala.jpg';
+
 
 function Feeds() {
-    const [feeds, setFeeds] = useState([]);
-    const [page, setPage] = useState(0);
-
-    const getFeeds = () => {
-        axios.post('/api/feeds/getallfeeds', null, {params: {page}})
-        .then(result => {
-            setFeeds([...feeds, ...result.data.feeds]);
-        })
-        .catch(err => {
-            console.error(err);
-        });
-    }
-
-    useEffect(() => {
-        getFeeds();
-    }, [page]);
-
 
     return (
         <>
@@ -31,12 +15,89 @@ function Feeds() {
                 <button className="link">Following</button>
             </div>
             <div className="wrap_feeds">
-                {   feeds.length ? (
-                    feeds.map((feed, feedIndex) => {return (
-                        <Feed feed={feed} key={feedIndex}/>
-                    );})
-                    ) : <div className="empty_feed_message">Feed가 없습니다.</div> 
-                }
+                <div className="feed">
+                    <div className="feed_head">
+                        <div>profileimg</div>
+                        <div>nickname</div>
+                        <div>timestamp</div>
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_content">ContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContentContent</div>
+                    <div className="feed_icon">123</div>
+                    <div className="feed_reply">456</div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #2
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #3
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #4
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #5
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #6
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #7
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #8
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #9
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
+                <div className="feed">
+                    <div className="feed_head">
+                        테스트 피드 #10
+                    </div>
+                    <div className="feed_img"><img src={Img} /></div>
+                    <div className="feed_icon"></div>
+                    <div className="feed_reply"></div>
+                </div>
             </div>
         </>
     )
