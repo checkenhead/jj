5856ce0160b0d4a897da19737270d76c951926d3
connie0714 <ok45909@gmail.com>
2024-03-14 18:36:50 +0900

    비밀번호 체크 비밀번호 수정 이메일 확인 js들 작성 feed Controller

--- jj/client/src/index.js
+++ jj/client/src/index.js
@@ -8,6 +8,8 @@ import './style/header.css';
 import './style/feeds.css';
 import './style/members/member.css';
 import './style/sub.css';
+import './style/members/join.css';
+import './style/members/login.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
