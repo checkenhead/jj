6c01050e136abb25dcd79a570aa9d0c8108fd982
connie0714 <ok45909@gmail.com>
2024-03-15 18:07:15 +0900

    dropdown 완성

---
+++ jj/client/src/component/feed/Dropdown.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import ImgRemove from '../../images/remove.png';
+import ImgEdit from '../../images/confirm.png';
+import axios from 'axios';
+
+function Dropdown(props) {
+    console.log(props);
+    const curpage = props.pagename;
+    const navigate = useNavigate();
+
+
+    function onDelete() {
+        let ans = window.confirm('정말로 삭제 하시겠습니까?');
+        console.log(props.feedid);
+        if (ans) {
+            axios.post('/api/feeds/deletebyid', { id: props.feedid })
+                .then(result => {
+                    navigate('/');
+                    return alert('게시물 삭제가 완료되었습니다');
+                })
+                .catch(error => {
+                    console.error(error);
+                })
+            navigate('/');
+        } else {
+            navigate('/');
+        }
+    }
+
+    return (
+        <div>
+            {
+                {
+                    feed:
+                        <div className='dropdown_table'>
+                            <div className='dropdown_button' onClick={() => { onDelete() }}>
+                                <img src={ImgRemove} />삭제
+                            </div>
+                            <div className='dropdown_button'>
+                                <img src={ImgEdit} />수정
+                            </div>
+                        </div>
+
+                }[curpage]
+            }
+        </div>
+    )
+}
+
+export default Dropdown
