c05323e4ff2f261209dda76f97ca02fc0e5e63aa
connie0714 <ok45909@gmail.com>
2024-03-18 14:40:04 +0900

    서머리js 네비게이트 추가

--- jj/client/src/component/feed/Summary.js
+++ jj/client/src/component/feed/Summary.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function Summary({ summarys }) {
+    const navigate = useNavigate();
     return (
         <div className="summary">
             {
