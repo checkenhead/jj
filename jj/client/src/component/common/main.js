a0d913fe5c8c067223c37a32231c312aada8d7de
Chicken Head <Chicken Head@DESKTOP-6RVSQM3.mshome.net>
2024-03-28 15:13:22 +0900

    scroll

--- jj/client/src/component/common/main.js
+++ jj/client/src/component/common/main.js
@@ -1,60 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useRef } from 'react'
 
 function Main({ component }) {
-    const root = document.getElementById('root');
+
     const currComponent = useRef();
-    const currScroll = useRef(0);
-    const scrollPositionRatio = useRef(0);
-
-    const syncScroll = () => {
-        const bodyScroll = document.documentElement.scrollTop;
-
-        currComponent.current.scrollTop += bodyScroll - currScroll.current;
-        currScroll.current = bodyScroll;
-    }
-
-    const setHeight = () => {
-        if(currComponent.current){
-            const rootHeight = Number(root.style.height.toString().replace('px', ''));
-
-            if (rootHeight < currComponent.current.scrollHeight) {
-                root.style.height = currComponent.current.scrollHeight.toString() + 'px';
-            }
-
-            const bodyScroll = document.documentElement.scrollTop;
-            currComponent.current.scrollTop += bodyScroll - currScroll.current;
-            currScroll.current = bodyScroll;
-        }
-    }
-
     
-
-    const handleResize = () => {
-        scrollPositionRatio.current = currScroll.current/document.documentElement.scrollHeight;
-        setHeight();
-        document.documentElement.scrollTop = document.documentElement.scrollHeight * scrollPositionRatio.current;
-        console.log("resized");
-    }
-
-    
-
-    useEffect(() => {
-        const inteval = setInterval(() => {
-            setHeight();
-        }, 100);
-
-        window.addEventListener('scroll', syncScroll);
-        window.addEventListener("resize", handleResize);
-
-        return (() => {
-            clearInterval(inteval);
-            window.removeEventListener('scroll', syncScroll);
-            window.removeEventListener("resize", handleResize);
-            document.documentElement.scrollTop = 0;
-            root.style.height = 0;
-        });
-    }, []);
-
     return (
         <main ref={currComponent}>
             {component}
