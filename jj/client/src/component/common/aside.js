4b3ee04b8846ddf6271b189ef451d532ff368dda
Chicken Head <Chicken Head@DESKTOP-6RVSQM3.mshome.net>
2024-03-28 16:10:09 +0900

    scroll

--- jj/client/src/component/common/aside.js
+++ jj/client/src/component/common/aside.js
@@ -5,34 +5,40 @@ function Aside({ component }) {
     const currComponent = useRef();
     const preScroll = useRef(0);
 
-    const getComponentTop = () => Number(currComponent.current.style.top.replace('px', ''));
+    const getComponentTop = () => Number(currComponent?.current?.style.top.replace('px', ''));
 
     const scrollDetector = () => {
-        const currScroll = window.scrollY;
-
-        if (preScroll.current < currScroll) {
-            currComponent.current.style.top = (getComponentTop() - (currScroll - preScroll.current)) + 'px';
-
-            if (getComponentTop() < (window.innerHeight - currComponent.current.scrollHeight)) {
-                currComponent.current.style.top = (window.innerHeight - currComponent.current.scrollHeight) + 'px';
-            }
-        } else {
-            currComponent.current.style.top = (getComponentTop() - (currScroll - preScroll.current)) + 'px';
-            if (getComponentTop() > 0) {
+        if(currComponent.current && currComponent.current.style){
+            if (currComponent?.current?.scrollHeight < window.innerHeight) {
                 currComponent.current.style.top = 0;
+                return;
             }
-            console.log(getComponentTop());
+            const currScroll = window.scrollY;
+    
+            if (preScroll.current < currScroll) {
+                currComponent.current.style.top = (getComponentTop() - (currScroll - preScroll.current)) + 'px';
+    
+                if (getComponentTop() < (window.innerHeight - currComponent.current.scrollHeight)) {
+                    currComponent.current.style.top = (window.innerHeight - currComponent.current.scrollHeight) + 'px';
+                }
+            } else {
+                currComponent.current.style.top = (getComponentTop() - (currScroll - preScroll.current)) + 'px';
+                if (getComponentTop() > 0) {
+                    currComponent.current.style.top = 0;
+                }
+                console.log(getComponentTop());
+            }
+    
+            preScroll.current = currScroll;
         }
-
-        preScroll.current = currScroll;
     }
 
     useEffect(() => {
         window.addEventListener('scroll', scrollDetector);
         return (() => {
-          window.removeEventListener('scroll', scrollDetector);
+            window.removeEventListener('scroll', scrollDetector);
         });
-      }, []);
+    }, []);
 
     return (
         <aside id="aside" ref={currComponent}>
